628d843c317c303be090ec46e960a277611c7772
kukaryamba <tavtolog@gmail.com>
2022-10-18 00:38:35 +0300

    feat: add delete digit solution

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,11 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function deleteDigit(n) {
+  let options = [];
+  let num = n.toString();
+  for (let i = 0; i < num.length; i++){
+    options.push(+(num.slice(0, i) + num.slice(i+1)));
+  }
+
+  return options.sort((a,b) => a - b)[options.length-1];
+
 }
 
+
 module.exports = {
-  deleteDigit
+deleteDigit
 };
+
